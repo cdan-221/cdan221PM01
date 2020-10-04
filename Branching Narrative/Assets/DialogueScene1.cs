fa6f2ca8ce07b993ae5087a3ff5b2acf0e2b4565
rowancorless <72184504+rowancorless@users.noreply.github.com>
2020-10-04 17:51:23 -0400

    Scene 1 Draft

---
+++ Branching Narrative/Assets/DialogueScene1.cs
@@ -0,0 +1,176 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class DialogueScene1 : MonoBehaviour
+{
+    public int primeInt = 1; // This integer drives game progress!
+    public Text Char1name;
+    public Text Char1speech;
+    public Text Char2name;
+    public Text Char2speech;
+    public Text Char3name;
+    public Text Char3speech;
+    public GameObject dialogue;
+    public GameObject ArtChar1;
+    public GameObject ArtBG1;
+    public GameObject Choice1a;
+    public GameObject Choice1b;
+    public GameObject NextScene1Button;
+    public GameObject NextScene2Button;
+    public GameObject nextButton;
+    //public GameObject gameHandler;
+    //public AudioSource audioSource;
+    private bool allowSpace = true;
+
+    void Start()
+    {         // initial visibility settings
+        dialogue.SetActive(false);
+        ArtChar1.SetActive(false);
+        ArtBG1.SetActive(true);
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        NextScene1Button.SetActive(false);
+        NextScene2Button.SetActive(false);
+        nextButton.SetActive(true);
+    }
+
+    void Update()
+    {         // use spacebar as Next button
+        if (allowSpace == true)
+        {
+            if (Input.GetKeyDown("space"))
+            {
+                talking();
+            }
+        }
+    }
+
+    public void talking()
+    {         // main story function. Players hit next to progress to next int
+        primeInt = primeInt + 1;
+        if (primeInt == 1)
+        {
+            // AudioSource.Play();
+        }
+        else if (primeInt == 2)
+        {
+            ArtChar1.SetActive(true);
+            dialogue.SetActive(true);
+            Char1name.text = "YOU";
+            Char1speech.text = "Hi Mom. I'm almost at work.";
+            Char2name.text = "";
+            Char2speech.text = "";
+        }
+        else if (primeInt == 3)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "MOM";
+            Char2speech.text = "I thought you said you were going to quit that job!";
+            //gameHandler.AddPlayerStat(1);
+        }
+        else if (primeInt == 4)
+        {
+            Char1name.text = "YOU";
+            Char1speech.text = "I need the money, Ma.";
+            Char2name.text = "";
+            Char2speech.text = "";
+        }
+        else if (primeInt == 5)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "MOM";
+            Char2speech.text = "It's too dangerous! You know how I worry!";
+            //gameHandler.AddPlayerStat(1);
+            nextButton.SetActive(false);
+            allowSpace = false;
+            Choice1a.SetActive(true); // function Choice1aFunct()
+            Choice1b.SetActive(true); // function Choice1bFunct()
+
+        }
+        else if (primeInt == 6)
+        {
+            Char1name.text = "YOU";
+            Char1speech.text = "placeholder.";
+            Char2name.text = "";
+            Char2speech.text = "";
+        }
+
+        // ENCOUNTER AFTER CHOICE #1
+        else if (primeInt == 100)
+        {
+            Char1name.text = "MOM";
+            Char1speech.text = "I'm calling your father to talk with you! Harold, speak some sense into your only child!";
+            Char2name.text = "";
+            Char2speech.text = "";
+        }
+        else if (primeInt == 101)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "YOU";
+            Char2speech.text = "What was that? Sorry Ma, you're breakin up. Gota go!";
+            nextButton.SetActive(false);
+            allowSpace = false;
+            NextScene1Button.SetActive(true);
+        }
+
+        else if (primeInt == 200)
+        {
+            Char1name.text = "MOM";
+            Char1speech.text = "OK, Dear. Just be safe. Don't get eaten.";
+            Char2name.text = "";
+            Char2speech.text = "";
+        }
+        else if (primeInt == 201)
+        {
+            Char1name.text = "";
+            Char1speech.text = "";
+            Char2name.text = "YOU";
+            Char2speech.text = "Bye, Ma. Love to Dad.";
+            nextButton.SetActive(false);
+            allowSpace = false;
+            NextScene1Button.SetActive(true);
+        }
+    }
+
+    // FUNCTIONS FOR BUTTONS TO ACCESS (Choice #1 and switch scenes)
+    public void Choice1aFunct()
+    {
+        Char1name.text = "";
+        Char1speech.text = "";
+        Char2name.text = "You";
+        Char2speech.text = "It's OK, Mom. I can handle it";
+        primeInt = 99;
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        nextButton.SetActive(true);
+        allowSpace = true;
+    }
+    public void Choice1bFunct()
+    {
+        Char1name.text = "";
+        Char1speech.text = "";
+        Char2name.text = "You";
+        Char2speech.text = "Don't worry... I'm planning to quit tonight.";
+        primeInt = 199;
+        Choice1a.SetActive(false);
+        Choice1b.SetActive(false);
+        nextButton.SetActive(true);
+        allowSpace = true;
+    }
+
+    public void SceneChange2a()
+    {
+        SceneManager.LoadScene("Scene2a");
+    }
+    public void SceneChange2b()
+    {
+        SceneManager.LoadScene("Scene2b");
+    }
+}
