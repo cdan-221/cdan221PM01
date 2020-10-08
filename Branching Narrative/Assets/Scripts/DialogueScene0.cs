f854dc4712e110ff7acfe68d07ca044fd9f53f86
tylerflahive <tmflahive@massart.edu>
2020-10-07 23:06:36 -0400

    Added menu and broken button

---
+++ Branching Narrative/Assets/Scripts/DialogueScene0.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class DialogueScene0 : MonoBehaviour
+{
+    public int primeInt = 1; // This integer drives game progress!
+
+    public GameObject ArtBG1;
+    public GameObject NextScene1Button;
+
+    //public GameObject gameHandler;
+    //public AudioSource audioSource;
+
+    void Start()
+    {         // initial visibility settings
+
+        ArtBG1.SetActive(true);
+        NextScene1Button.SetActive(true);
+    }
+
+    public void SceneChange1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+
+}
